4e741500095846339742aaeba58413cb174aa215
FHernandez08 <fidelhernandez_8@hotmail.com>
2025-08-14 16:04:32 -0500

    folder structure created

---
+++ server/index.js
@@ -0,0 +1,28 @@
+import express from 'express';
+import cors from 'cors';
+import morgan from 'morgan';
+import dotenv from 'dotenv';
+
+dotenv.config();
+
+const app = express();
+const PORT = process.env.PORT || 5000;
+
+app.use(cors({
+    origin: process.env.CLIENT_URL,
+    credentials: true,
+}));
+
+app.use(express.json());
+app.use(morgan('dev')); // logs HTTP requests
+
+app.get("/" , (req, res) => {
+    res.send("Server is running!");
+});
+
+
+
+// START SERVER
+app.listen(PORT, () => {
+    console.log(`Server running on http://localhost:${PORT}`);
+});
