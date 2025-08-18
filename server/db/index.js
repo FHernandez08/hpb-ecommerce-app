5b815da7a48830c18a9eba80a7a6576de7fdea4f
FHernandez08 <fidelhernandez_8@hotmail.com>
2025-08-18 16:25:15 -0500

    connecting to the database for app

---
+++ server/db/index.js
@@ -0,0 +1,11 @@
+import pg from "pg";
+
+const db = new pg.Client({
+    user: 'postgres',
+    host: 'localhost',
+    database: 'ecommerce',
+    password: 'Astros#0213',
+    port: 5433,
+});
+
+db.connect();
