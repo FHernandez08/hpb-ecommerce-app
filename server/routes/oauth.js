1db901f478fcbbad03d6e73690c480d2b39f26b4
Fidel Hernandez <fidelhernandez_8@hotmail.com>
2025-09-15 23:05:44 -0500

    adding oauth data

---
+++ server/routes/oauth.js
@@ -0,0 +1,16 @@
+const express = require("express");
+const passport = require("passport");
+const FacebookStrategy = require("passport-facebook").Strategy;
+const GoogleStrategy = require("passport-google-oauth20").Strategy;
+const MicrosoftStrategy = require("passport-microsoft").Strategy;
+
+const router = express.Router();
+
+
+// Google OAuth Functionality
+
+
+// Facebook OAuth Functionality
+
+
+// Microsoft OAuth Functionality
