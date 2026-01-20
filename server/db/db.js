7b69721a934a398bacef553f4dbc19ed74a7d684
Fidel Hernandez <fidelhernandez_8@hotmail.com>
2026-01-19 18:08:21 -0600

    updated the database to connect to backend deployment

--- server/db/db.js
+++ server/db/db.js
@@ -1,10 +1,16 @@
 import { Pool } from "pg";
 import "dotenv/config";
 
-export const pool = new Pool({
-    user: process.env.PG_USER,
-    host: process.env.PG_HOST,
-    database: process.env.PG_DATABASE,
-    password: process.env.PG_PASSWORD,
-    port: Number(process.env.PG_PORT),
-});
+export const pool = new Pool(
+    process.env.DATABASE_URL
+    ? {
+        connectionString: process.env.DATABASE_URL,
+        ssl: { rejectUnauthorized: false },
+    } : {
+        user: process.env.PG_USER,
+        host: process.env.PG_HOST,
+        database: process.env.PG_DATABASE,
+        password: process.env.PG_PASSWORD,
+        port: Number(process.env.PG_PORT)
+    }
+)
