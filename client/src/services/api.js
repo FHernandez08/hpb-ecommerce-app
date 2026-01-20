ee6b7ef119499b8c5439c28e7115b9901f9faad2
Fidel Hernandez <fidelhernandez_8@hotmail.com>
2026-01-20 08:35:22 -0600

    resolving cookie issues

--- client/src/services/api.js
+++ client/src/services/api.js
@@ -1,10 +1,8 @@
-import axios from 'axios';
+import axios from "axios";
 
 const api = axios.create({
-    baseURL: import.meta.env.VITE_API_URL || "/api",
-    withCredentials: true,
-})
-
-console.log("API BASE URL =", import.meta.env.VITE_API_URL);
+  baseURL: "/api",
+  withCredentials: true,
+});
 
 export default api;
